1d281d6ea3c0679d29cf99c4b4fd8fd7dde718f2
Shubham Mishra <bug9369@gmail.com>
2023-09-17 22:42:00 +0530

    Update questionRoute.js

--- routes/questionRoute.js
+++ routes/questionRoute.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router();
 
-const { getAllQuestions, createQuestion, updateQuestion, deleteQuestion, getAquestion } = require('../controllers/questionsController');
+const { allSubjectsName,searchBySubjectName,getAllQuestions, createQuestion, updateQuestion, deleteQuestion, getAquestion } = require('../controllers/questionsController');
 const { isAuthenticatedAdmin,authorizedAdminRoles } = require('../middleware/authentication');
 
+router.route('/questions/all-subjects').get(allSubjectsName);
+router.route('/questions/all-subjects/:subject').get(searchBySubjectName);
 router.route('/questions').get(getAllQuestions);
 router.route('/admin/questions/new').post(isAuthenticatedAdmin,authorizedAdminRoles("admin","writer"),createQuestion);
 router.route('/admin/questions/:id').put(isAuthenticatedAdmin,authorizedAdminRoles("admin","writer"),updateQuestion).delete(isAuthenticatedAdmin,authorizedAdminRoles("admin"),deleteQuestion);
