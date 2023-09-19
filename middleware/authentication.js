357187189f6f771a7a7e181c342ae43deea56d27
Shubham Mishra <bug9369@gmail.com>
2023-09-20 00:21:37 +0530

    Update authentication.js

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -9,7 +9,7 @@ const jwt = require("jsonwebtoken");
 exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
   const userToken = await req.header("userToken");
   if (!userToken) {
-    return next(new ErrorHandler(401, "Please login to access this feature"));
+    return next(new ErrorHandler(401, "Please login to access this feature."));
   }
   const isValidToken = await jwt.verify(userToken, process.env.JWT_SECRET);
   req.user = await User.findById({ _id: isValidToken.id });
@@ -23,7 +23,7 @@ exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
 exports.isAuthenticatedAdmin = catchAsyncErrors(async (req, res, next) => {
   const adminToken  = await req.header("adminToken");
   if (!adminToken) {
-    return next(new ErrorHandler(401, "Please login to access this feature"));
+    return next(new ErrorHandler(401, "Please login to access this feature."));
   }
   const isValidToken = await jwt.verify(adminToken,"This_is_secret_key");
   req.admin = await Admin.findById({ _id: isValidToken.id });
@@ -38,7 +38,7 @@ exports.authorizedAdminRoles = (...roles) => {
       next(
         new ErrorHandler(
           401,
-          `${String(req.admin.role).toUpperCase()} is not allowed to access this resource`
+          `${String(req.admin.role).toUpperCase()} is not authorized.`
         )
       );
     }
